aa83a5a05e0fe89ff54f90317e0b70e6525607b8
LDoorlag <ljdoorlag@icloud.com>
2024-11-25 11:30:16 -0600

    updated doc strings

--- television.py
+++ television.py
@@ -6,81 +6,82 @@ class Television():
     MAX_VOLUME: int = 2
     MIN_CHANNEL: int = 0
     MAX_CHANNEL:int = 3
-    def __init__(self, status=False, muted=False, volume=0, channel=0):
+    def __init__(self, status=False, muted=False, volume=0, channel=0) -> None:
+        
         self.__status: bool = status
         self.__volume: int = volume
         self.__channel: int = channel
         self.__muted: bool = muted
-    def power(self):
+    def power(self) -> None:
         '''
-        This method will turn the television on or off by switching status to True or False.
-        no function if power off
+        power function turns on or off the television by switching the status var to True or False.
+        :param None:
+        :return: None
         '''
         if self.__status == False:
             self.__status = True
         else:
             self.__status = False
-    def mute (self):
+    def mute (self) -> None:
         '''
-        This method will mute the television if it is not muted, and unmute the television if it is muted.
-        It switches the muted var to True or False.
-        no function if power off
+        mute function changes the muted var of the television to True or False. Does not work if tv power is off
+        :param None:
+        :return: None
         '''
-        if self.__status == False:
-            return  
-        if self.__muted == False:
-            self.__muted = True
-        else:
-            self.__muted = False
-    def channel_up(self):
+        if self.__status == True:
+            if self.__muted == False:
+                self.__muted = True
+            else:
+                self.__muted = False
+    def channel_up(self) -> None:
         '''
-        increased channel by 1 or if at max channel, reset to min channel
-        no function if power off
+        channel_up function increases the channel by 1. If channel is at max channel, reset to min channel. Does not function if power is off.
+        :param None:
+        :return: None
         '''
-        if self.__status == False:
-            return
-        if self.__channel < self.MAX_CHANNEL:
-            self.__channel += 1
-        else:
-            self.__channel = self.MIN_CHANNEL
-    def channel_down(self):
+        if self.__status == True:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+    def channel_down(self) -> None:
         '''
-        decreased channel by 1 or if at min channel, reset to max channel
-        no function if power off
+        channel_down function decreases channel by 1 or if at min channel, reset to max channel. Does not function if power is off.
+        :param None:
+        :return: None
         '''
-        if self.__status == False:
-            return
-        if self.__channel > self.MIN_CHANNEL:
-            self.__channel -= 1
-        else:
-            self.__channel = self.MAX_CHANNEL
-    def volume_up(self):
+        if self.__status == True:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+    def volume_up(self) -> None:
         '''
-        increases volume by 1. If at max volume, does nothing.
-        no function if power off
+        volume_up function increases volume by 1. If at max volume, does nothing. does not function if power off
+        :param None:
+        :return: None
         '''
-        if self.__status == False:
-            return
-        if self.__volume < self.MAX_VOLUME:
-            self.__volume += 1
-        if self.__muted == True:
-            self.__muted = False
-    def volume_down(self):
+        if self.__status == True:
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+            if self.__muted == True:
+                self.__muted = False
+    def volume_down(self) -> None:
         '''
-        decreses volume by 1. If at min volume, does nothing.
-        no function if power off
+        volume_down function decreases volume by 1. If at min volume, does nothing. does not function if power off
+        :param None:
+        :return: None
         '''
-        if self.__status == False:
-            return
-        if self.__volume > self.MIN_VOLUME:
-            self.__volume -= 1
-        if self.__muted == True:
-            self.__muted = False
-    def __str__(self):
+        if self.__status == True:
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+            if self.__muted == True:
+                self.__muted = False
+    def __str__(self) -> str:
         '''
-        This method will return a string representation of the Television object.
-        If the television is muted, the volume will be 0.
-        does function if power off
+        This method will return a string representation of the Television object. If the television is muted, the volume will be 0. DOES function if power off
+        :param None:
+        :return: str
         '''
         if self.__muted == True:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
